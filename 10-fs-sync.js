37ab1abbdfefc7f807b1a963ca183a9d0a3d5f7b
Anish0999 <anishgupta0999@gmail.com>
2024-09-24 10:21:33 -0400

    first commit

---
+++ 10-fs-sync.js
@@ -0,0 +1,20 @@
+const { readFileSync, writeFileSync, writeFile } = require("fs");
+//const fs = require('fs');
+
+console.log('start');
+
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+//console.log(first, second);
+
+// two values, (file name, value to pass)
+
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result: ${first}, ${second}`,
+  { flag: 'a' }
+);
+
+console.log('done with this task');
+console.log('starting the next one');
